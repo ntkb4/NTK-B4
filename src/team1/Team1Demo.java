eaf4786cc4a48b13b0f0d7e4cfb6346a17fa8ced
Yousuf Zia <Yousuf Zia@10.0.0.180>
2021-10-03 15:01:44 -0600

    Wagma initial commit

--- src/team1/Team1Demo.java
+++ src/team1/Team1Demo.java
@@ -5,5 +5,10 @@ public class Team1Demo {
 	public static void main(String[] args) {
 		System.out.println("Team 1");
 	}
+	
+	public void subtractNum(int a, int b) {
+		
+		System.out.println(a-b);
+	}
 
 }
