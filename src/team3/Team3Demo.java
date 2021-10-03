1dafae018214595b5b38659a70352fc48374d7eb
Administrator <Administrator@Shams_Khalid.mshome.net>
2021-10-03 12:25:05 -0700

    AN

--- src/team3/Team3Demo.java
+++ src/team3/Team3Demo.java
@@ -5,6 +5,12 @@ public class Team3Demo {
 	public static void main(String[] args) {
 		System.out.println("Team 3");
 		//don't forget to pull
+		
+		
+	}
+	
+	public void display() {
+		System.out.println(" welcome team 3");
 	}
 
 }
